36ce671d37f227d567834d6272f0be2f3a57f977
Soneca <62015770+S0NES@users.noreply.github.com>
2020-06-02 16:33:29 -0300

    server 0.2

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,12 @@ const app = express();
 
 app.get('/users', (request, response) => {
     console.log("Listagem de usuarios");
+    response.json([
+        "Diego",
+        "Daniel",
+        "Cleiton",
+        "Robson"
+    ]);
 });
 
 app.listen(3333);
