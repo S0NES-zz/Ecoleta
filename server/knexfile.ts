4d84a2000272e17fa5555b4f8bf1cb0d2eda2f85
S0NES <scarabelli.vinicius@gmail.com>
2020-06-03 19:30:45 -0300

    server 0.42

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,11 +1,15 @@
-import path from 'path';
+import path from "path";
+
 module.exports = {
-  client: "sqlite3",
-  connection: ({
-    filname: path.resolve(__dirname, "src", "database", "database.sqlite"),
-  }),
-  migrations: ({
-    directory: path.resolve(__dirname, "src", "database", "migrations"),
-  }),
-  useNullAsDefault: true,
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: path.resolve(__dirname, "src", "database", "database.sqlite"),
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
+    },
+    useNullAsDefault: true,
+  }
+
 };
