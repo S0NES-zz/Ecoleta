927893cd5f9eb9ee209cb5a304cc314bd795f850
S0NES <scarabelli.vinicius@gmail.com>
2020-06-04 10:14:49 -0300

    server 0.65

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import knex from './database/connection';
 
 const routes = express.Router();
@@ -6,8 +6,9 @@ const routes = express.Router();
 routes.get('/items', async (request, response) => {
     const items = await knex('items').select('*');
 
-    const serializedItems = items.map(item =>{
+    const serializedItems = items.map(item =>{ 
         return {
+          id: item.id,
           title: item.title,
           image_url: `http://localhost:3333/uploads/${item.image}`,
         };
@@ -17,4 +18,28 @@ routes.get('/items', async (request, response) => {
 
 });
 
+routes.post('/points', async (request, response) =>{
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+
+  await knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+  return response.json({ succes : true});
+});
 export default routes;
