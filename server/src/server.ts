46388a86c6aa5046eb8e0570375a1f8192a6b487
S0NES <scarabelli.vinicius@gmail.com>
2020-06-05 15:41:04 -0300

    server 0.92

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,20 +13,3 @@ app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname,  '..', 'uploads')));
 
 app.listen(3333);
-
-
-//Rota: Endereço completo da requisição
-//Recurso: Qual Entidade estamos acessando do sistema
-
-//GET: Buscar uma oumais informações no back-end
-//POST: Criar uma nova informaçãono back-end
-//PUT: Atualizar uma ou mais informações no back-end
-//DELETE: Remover uma informação no back-end
-
-//POST: http://localhost:3333/users = Criar um Usuário
-//GET: http://localhost:3333/users = Listar usuário
-//GET: http://localhost:3333/users/5 = Buscar dados do usuário com ID 5
-
-
-//Request Param = Parâmetros que vem na própria rota que identificam um recurso
-//Query Param = Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação..
